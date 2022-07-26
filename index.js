cbf4f978a82f1e8ac3127c37679cca6236b0c9aa
mmuhaiminul-islam-bjit <muhaiminul.islam@bjitgroup.com>
2022-07-27 01:07:53 +0600

    added file

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const http = require('http');
-
-const hostname = 'localhost';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Everyone !\nThis is my first app on azure. Thank you for visiting');
+  const msg = 'Hello Node!\n'
+  res.end(msg);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
 });
