5828f988aca151a07fa75960b3bd71f8446b944b
mmuhaiminul-islam-bjit <muhaiminul.islam@bjitgroup.com>
2022-08-17 10:45:24 +0600

    firsst test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Final check\n'
+  const msg = 'Checking with ssh\n'
   res.end(msg);
 });
 
